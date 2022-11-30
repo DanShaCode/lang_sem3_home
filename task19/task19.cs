3b94983055b9d4aadbe6fc4f0f2beb100560b390
Danis Sharipov <danshadance@mail.ru>
2022-11-30 11:22:32 -0800

    Поработал над задачей19

--- task19/task19.cs
+++ task19/task19.cs
@@ -1,1 +1,25 @@
-﻿
+﻿// Принимаем на вход пятизначное число 
+// Проверяем является ли число полиндромом
+
+// Пример числа полиндрома - 121; 676; 1331; 4884; 94949;
+
+int number5 = Convert.ToInt32(Console.ReadLine());
+
+if (number5 < -99999 | number5 > -10000 && number5 < 10000 | number5 > 99999)
+{
+    Console.WriteLine("Input error. Enter a five-digit number.");
+}
+
+
+
+// int num1 = number5 / 10000;
+
+// int num2 = (number5 / 1000) % 10;
+
+// int num3 = (number5 / 100) % 10;
+
+// int num4 = (number5 / 10) % 10;
+
+// int num5 = number5 % 10;
+
+// Console.WriteLine(num5);
